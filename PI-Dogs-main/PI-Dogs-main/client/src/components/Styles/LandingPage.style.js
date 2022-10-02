4d6e9dd8de64a0620b9dae41a6fc7b1c29fe90d5
Alejo Ufano <ufanoalejo@gmail.com>
2022-10-02 14:37:22 -0300

    Fix(LandingPage) fixes & changes (responsive)

--- PI-Dogs-main/PI-Dogs-main/client/src/components/Styles/LandingPage.style.js
+++ PI-Dogs-main/PI-Dogs-main/client/src/components/Styles/LandingPage.style.js
@@ -7,14 +7,23 @@ flex-direction: column;
 height: 100vh;
 background: url('https://i.imgur.com/1CpfWIA.jpg');
 background-repeat: no-repeat;
-background-position: right;
+background-position: left;
 background-size: cover; 
 transition: all 0.4s 0s ease;
+@media screen and (max-width: 1000px) {
+    background-position: center;
+}
 `
 export let LandingContainer = styled.div`
 display: flex;
 align-items: center;
 height: 100vh;
+@media screen and (max-width: 500px) {
+   flex-direction: column-reverse;
+   justify-content: center;
+   align-items: flex-start;
+   padding-left: .1rem;
+}
 `
 export let ContactButtons = styled.div`
 display: flex;
@@ -25,6 +34,13 @@ justify-content: space-around;
 justify-self: center;
 margin-left: 3vw;
 padding: .2vw;
+@media screen and (max-width: 500px) {
+    flex-direction: row;
+    height: max-content;
+    width: 30vw;
+    margin-top: 2rem;
+    margin-left: 1rem;
+}
 `
 export let LandingTextDiv = styled.div`
 display: flex;
@@ -38,7 +54,7 @@ color: #dedede;
 text-decoration: none;
 border: none;
 border-radius: 30px;
-font-size: 17px;
+font-size: 1rem;
 margin: .5vw;
 padding: 8px 45px;
 background-color: #ff3366;
@@ -51,11 +67,17 @@ transition: all 400ms cubic-bezier(.23, 1, 0.32, 1);
     color: #fff;
     transform: scale(1.05);
 }
+@media screen and (max-width: 700px) {
+    font-size: .8rem;
+}
+@media screen and (max-width: 400px) {
+    font-size: .7rem;
+}
 `
 export let LandingPreSlogan = styled.div`
 display: flex;
 font-family: 'Montserrat', sans-serif;
-font-size: 30px;
+font-size: 1.8rem;
 margin: .5vw;
 transition: all 0.4s 0s ease;
 text-shadow: 5px 10px 15px rgb(0 0 0 / 20%);
@@ -68,15 +90,36 @@ span{
         text-shadow: 5px 10px 15px rgb(255 51 132 / 50%);
     }
 }
+@media screen and (max-width: 800px) {
+    font-size: 1.4rem;
+}
+@media screen and (max-width: 600px) {
+    font-size: 1.2rem;
+}
+@media screen and (max-width: 500px) {
+    font-size: 1rem;
+}
 `
 export let LandingSlogan = styled.div`
 font-family: 'Work Sans', sans-serif;
 font-weight: 900;
 margin: .5vw;
-font-size: 93px;
+font-size: 6rem;
 color: #000;
 cursor: default;
 text-shadow: 5px 10px 15px rgb(0 0 0 / 20%);
+@media screen and (max-width: 800px) {
+    font-size: 5rem;
+}
+@media screen and (max-width: 600px) {
+    font-size: 4rem;
+}
+@media screen and (max-width: 500px) {
+    font-size: 3rem;
+}
+@media screen and (max-width: 400px) {
+    font-size: 2.5rem;
+}
 `
 
 export let Icon = styled.div``
